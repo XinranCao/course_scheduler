3251a3f479102803167add574e567dcda5ef8c82
Xinran Cao <xinrancao@hotmail.com>
2021-09-21 10:20:17 +0800

    state control

--- src/newVersion/Scheduler.js
+++ src/newVersion/Scheduler.js
@@ -6,127 +6,12 @@ import './newVersion.css';
 function Scheduler (props) {
 
   const { favList, modifyFavList } = props
-  const [selectedCourseList, setselectedCourseList] = useState({})
-
-  useEffect(()=> {
-    let newList = JSON.parse(JSON.stringify(selectedCourseList))
-
-    Object.entries(selectedCourseList).map( ([selectedCourse, courseInfo]) => {
-      if( Object.keys(favList).includes(selectedCourse) ) {
-        Object.entries(courseInfo.sections).map(([ selectedSec, sectionInfo ])=>{
-          if(Object.keys(favList[selectedCourse].sections).includes(selectedSec)) {
-            Object.entries(sectionInfo.subsections).map(([ selectedSubSec ])=>{
-              if(!Object.keys(favList[selectedCourse].sections[selectedSec].subsections).includes(selectedSubSec)) {
-                delete newList[selectedCourse].sections[selectedSec].subsections[selectedSubSec]
-              }
-              return null
-            })
-          } else {
-            delete newList[selectedCourse].sections[selectedSec]
-          }
-          return null
-        })
-      } else {
-        delete newList[selectedCourse]
-      }
-      return null
-    })
-    setselectedCourseList(newList)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[favList])
-
-  const handleModifyAllSelected = (operation) => {
-    let newList = {}
-    if (operation === 'add') {
-      newList = JSON.parse(JSON.stringify(favList))
-    }
-    setselectedCourseList(newList)
-  }
-
-  const handleModifySelectedCourseList = (operation, key, course, sectionNum, subSectionNum) => {
-
-    const courseInfo = JSON.parse(JSON.stringify(course))
-    const newList = JSON.parse(JSON.stringify(selectedCourseList))
-
-    if (operation === 'remove') {
-      if (key === 'all') {
-        // delete whole course
-        delete newList[courseInfo.number];
-      } else if (key === 'section') {
-        // delete section
-        delete newList[courseInfo.number].sections[sectionNum];
-        var sectionObj = newList[courseInfo.number].sections;
-        // when all sections are deleted, delete the course
-        if (Object.entries(sectionObj).length === 0 && sectionObj.constructor === Object) {
-          delete newList[courseInfo.number];
-        }
-      } else {
-        // delete subsection
-        delete newList[courseInfo.number].sections[sectionNum].subsections[subSectionNum];
-        var subsectionObj = newList[courseInfo.number].sections[sectionNum].subsections;
-        // when all subsections are deleted, delete the section
-        if (Object.entries(subsectionObj).length === 0 && subsectionObj.constructor === Object) {
-          delete newList[courseInfo.number].sections[sectionNum];
-          let sectionObj = newList[courseInfo.number].sections;
-          // when all ections a deleted, delete the course
-          if (Object.entries(sectionObj).length === 0 && sectionObj.constructor === Object)
-            delete newList[courseInfo.number];
-        }
-      }
-      setselectedCourseList(newList)
-      return
-    }
-
-    if (key === 'all') {
-      newList[courseInfo.number] = {...courseInfo}
-    } else if (key === 'section') {
-      // check if the course is already in schedule
-      if (newList[courseInfo.number]) {
-          (newList[courseInfo.number].sections)[sectionNum] = courseInfo.sections[sectionNum]
-      } else {
-        newList[courseInfo.number] = {
-          ...courseInfo,
-          "sections": { [sectionNum]: courseInfo.sections[sectionNum] }
-        }
-      }
-    } else {
-      // check if the course is already in schedule
-      if (newList[courseInfo.number]) {
-        let sections = newList[courseInfo.number].sections;
-        //check if the section is already in schedule
-        if (sections[sectionNum]) {
-          sections[sectionNum].subsections[subSectionNum] = courseInfo.sections[sectionNum].subsections[subSectionNum];
-        }
-        else {
-          sections[sectionNum] = {
-            ...courseInfo.sections[sectionNum],
-            "subsections": { [subSectionNum]: courseInfo.sections[sectionNum].subsections[subSectionNum] }
-          }
-        }
-      } else {
-        newList[courseInfo.number] =
-        {
-          ...courseInfo,
-          "sections": {
-            [sectionNum]: {
-              ...courseInfo.sections[sectionNum],
-              "subsections": { [subSectionNum]: courseInfo.sections[sectionNum].subsections[subSectionNum] }
-            }
-          }
-        }
-      }
-    }
-    setselectedCourseList(newList)
-  }
 
   return <>
     <FavList
       favList = {favList} 
-      modifyFavList = {modifyFavList}      
-      selectedCourseList={selectedCourseList}
-      modifyAllSelected={handleModifyAllSelected}
-      modifySelectedCourseList={ (operation, key, course, sectionNum, subSectionNum) => handleModifySelectedCourseList(operation, key, course, sectionNum, subSectionNum) } />
-    <Schedule selectedCourseList={selectedCourseList} />
+      modifyFavList = {modifyFavList} />
+    <Schedule />
   </>
 }
 
--- src/newVersion/schedulerComp/FavList.js
+++ src/newVersion/schedulerComp/FavList.js
@@ -3,73 +3,101 @@ import '../newVersion.css';
 
 function FavList(props) {  
 
-  const { 
-    favList, 
-    modifyFavList, 
-    selectedCourseList,
-    modifyAllSelected, 
-    modifySelectedCourseList
-  } = props
+  const { favList, modifyFavList } = props
 
-  const [showList, setShowList] = useState({
+  const [state, setState] = useState({
     showSecList: [],
-    showSubSecList: []
+    showSubSecList: [],
+    selectedCourseList: {}
   })
 
   useEffect(()=>{
     let newShowSecList = []
     let newShowSubSecList = []
 
-    showList.showSecList.map( course => {
+    state.showSecList.map( course => {
       if (Object.keys(favList).includes(course)) {
-        newShowSecList = [...showList.showSecList]
-        showList.showSubSecList.map( course_sec => {
+        newShowSecList = [...state.showSecList]
+        state.showSubSecList.map( course_sec => {
           if (!Object.keys(favList[course].sections).includes(course_sec.split('-')[1])) {
-            newShowSubSecList = showList.showSubSecList.filter( e => e !== course_sec )
+            newShowSubSecList = state.showSubSecList.filter( e => e !== course_sec )
           } else {
-            newShowSubSecList = [...showList.showSubSecList]
+            newShowSubSecList = [...state.showSubSecList]
           }
           return null
         })
       } else {
-        newShowSecList = showList.showSecList.filter( e => e !== course )
-        newShowSubSecList = showList.showSubSecList.filter( e => e.indexOf(course) < 0 )
+        newShowSecList = state.showSecList.filter( e => e !== course )
+        newShowSubSecList = state.showSubSecList.filter( e => e.indexOf(course) < 0 )
       }
       return null
     })
 
-    setShowList({ 
+    let newSelectedCourseList = JSON.parse(JSON.stringify(state.selectedCourseList))
+
+    Object.entries(state.selectedCourseList).map( ([selectedCourse, courseInfo]) => {
+      if( Object.keys(favList).includes(selectedCourse) ) {
+        Object.entries(courseInfo.sections).map(([ selectedSec, sectionInfo ])=>{
+          if(Object.keys(favList[selectedCourse].sections).includes(selectedSec)) {
+            Object.entries(sectionInfo.subsections).map(([ selectedSubSec ])=>{
+              if(!Object.keys(favList[selectedCourse].sections[selectedSec].subsections).includes(selectedSubSec)) {
+                delete newSelectedCourseList[selectedCourse].sections[selectedSec].subsections[selectedSubSec]
+              }
+              return null
+            })
+          } else {
+            delete newSelectedCourseList[selectedCourse].sections[selectedSec]
+          }
+          return null
+        })
+      } else {
+        delete newSelectedCourseList[selectedCourse]
+      }
+      return null
+    })
+
+    setState({ 
       showSecList: newShowSecList,
-      showSubSecList: newShowSubSecList
+      showSubSecList: newShowSubSecList,
+      selectedCourseList: newSelectedCourseList
     })
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [favList])
 
+  const modifyAllSelected = (operation) => {
+    let newList = {}
+    if (operation === 'add') {
+      newList = JSON.parse(JSON.stringify(favList))
+    }
+    setState({ ...state, selectedCourseList: newList })
+  }
+
   const toggleShowSecList = (course) => {
-    if( showList.showSecList.indexOf(course) < 0 ) {
-      setShowList({
-        ...showList,
-        showSecList: [...showList.showSecList, course]
+    if( state.showSecList.indexOf(course) < 0 ) {
+      setState({
+        ...state,
+        showSecList: [...state.showSecList, course]
       })
     } else {
-      setShowList({
-        showSecList: showList.showSecList.filter( e => e !== course ),
-        showSubSecList: showList.showSubSecList.filter( e => e.indexOf(course) < 0 )
+      setState({
+        ...state,
+        showSecList: state.showSecList.filter( e => e !== course ),
+        showSubSecList: state.showSubSecList.filter( e => e.indexOf(course) < 0 )
       })
     }
   }
 
   const toggleShowSubSecList = (course, section) => {
     const course_sec = course + '-' + section
-    if( showList.showSubSecList.indexOf(course_sec) < 0 ) {
-      setShowList({
-        ...showList,
-        showSubSecList: [...showList.showSubSecList, course_sec]
+    if( state.showSubSecList.indexOf(course_sec) < 0 ) {
+      setState({
+        ...state,
+        showSubSecList: [...state.showSubSecList, course_sec]
       })
     } else {
-      setShowList({
-        ...showList,
-        showSubSecList: showList.showSubSecList.filter( e => e !== course_sec )
+      setState({
+        ...state,
+        showSubSecList: state.showSubSecList.filter( e => e !== course_sec )
       })
     }
   }
@@ -94,18 +122,18 @@ function FavList(props) {
   const checkAllCoursesSelected = () => {
     let flag = true
     for (const [course, courseInfo] of Object.entries(favList) ) {
-      const courseSelected = selectedCourseList[course]
+      const courseSelected = state.selectedCourseList[course]
       if (!courseSelected) {
         flag = false
         return
       }
       for (const [section, sectionInfo] of Object.entries(courseInfo.sections)) {
-        if (!selectedCourseList[course].sections[section]) {
+        if (!state.selectedCourseList[course].sections[section]) {
           flag = false
           return
         }
         for (const [subsection] of Object.entries(sectionInfo.subsections)) {
-          if (!selectedCourseList[course].sections[section].subsections[subsection]) {
+          if (!state.selectedCourseList[course].sections[section].subsections[subsection]) {
             flag = false
             return
           }
@@ -115,9 +143,85 @@ function FavList(props) {
     return flag
   }
 
+  const modifySelectedCourseList = (operation, key, course, sectionNum, subSectionNum) => {
+
+    const courseInfo = JSON.parse(JSON.stringify(course))
+    const newList = JSON.parse(JSON.stringify(state.selectedCourseList))
+
+    if (operation === 'remove') {
+      if (key === 'all') {
+        // delete whole course
+        delete newList[courseInfo.number];
+      } else if (key === 'section') {
+        // delete section
+        delete newList[courseInfo.number].sections[sectionNum];
+        var sectionObj = newList[courseInfo.number].sections;
+        // when all sections are deleted, delete the course
+        if (Object.entries(sectionObj).length === 0 && sectionObj.constructor === Object) {
+          delete newList[courseInfo.number];
+        }
+      } else {
+        // delete subsection
+        delete newList[courseInfo.number].sections[sectionNum].subsections[subSectionNum];
+        var subsectionObj = newList[courseInfo.number].sections[sectionNum].subsections;
+        // when all subsections are deleted, delete the section
+        if (Object.entries(subsectionObj).length === 0 && subsectionObj.constructor === Object) {
+          delete newList[courseInfo.number].sections[sectionNum];
+          let sectionObj = newList[courseInfo.number].sections;
+          // when all ections a deleted, delete the course
+          if (Object.entries(sectionObj).length === 0 && sectionObj.constructor === Object)
+            delete newList[courseInfo.number];
+        }
+      }
+      setState({ ...state, selectedCourseList: newList })
+      return
+    }
+
+    if (key === 'all') {
+      newList[courseInfo.number] = {...courseInfo}
+    } else if (key === 'section') {
+      // check if the course is already in schedule
+      if (newList[courseInfo.number]) {
+          (newList[courseInfo.number].sections)[sectionNum] = courseInfo.sections[sectionNum]
+      } else {
+        newList[courseInfo.number] = {
+          ...courseInfo,
+          "sections": { [sectionNum]: courseInfo.sections[sectionNum] }
+        }
+      }
+    } else {
+      // check if the course is already in schedule
+      if (newList[courseInfo.number]) {
+        let sections = newList[courseInfo.number].sections;
+        //check if the section is already in schedule
+        if (sections[sectionNum]) {
+          sections[sectionNum].subsections[subSectionNum] = courseInfo.sections[sectionNum].subsections[subSectionNum];
+        }
+        else {
+          sections[sectionNum] = {
+            ...courseInfo.sections[sectionNum],
+            "subsections": { [subSectionNum]: courseInfo.sections[sectionNum].subsections[subSectionNum] }
+          }
+        }
+      } else {
+        newList[courseInfo.number] =
+        {
+          ...courseInfo,
+          "sections": {
+            [sectionNum]: {
+              ...courseInfo.sections[sectionNum],
+              "subsections": { [subSectionNum]: courseInfo.sections[sectionNum].subsections[subSectionNum] }
+            }
+          }
+        }
+      }
+    }
+    setState({ ...state, selectedCourseList: newList })
+  }
+
   const getTotalCredits = () => {
     let totalCretis = 0
-    Object.values(selectedCourseList).map(courseInfo => {
+    Object.values(state.selectedCourseList).map(courseInfo => {
       totalCretis += courseInfo.credits
       return null
     })
@@ -147,7 +251,7 @@ function FavList(props) {
       }
       {
         Object.values(favList).map( course => {
-          const courseSelected = selectedCourseList[course.number]
+          const courseSelected = state.selectedCourseList[course.number]
           return <React.Fragment key={course.number}>
             <div className='favCourse'>
               <div 
@@ -155,10 +259,11 @@ function FavList(props) {
                 onClick={ () => modifySelectedCourseList( courseSelected ? 'remove' : 'add', 'all', course, null, null) }>
                   {courseSelected ? '✓' : ''}
               </div>
+              {console.log('state',state)}
               <div 
-                className={ showList.showSecList.includes(course.number) ? 'showSectionBtn' : 'showSectionBtn_active' }
+                className={ state.showSecList.includes(course.number) ? 'showSectionBtn' : 'showSectionBtn_active' }
                 onClick={()=>toggleShowSecList(course.number)}>
-                  {showList.showSecList.includes(course.number) ? 'Hide sections' : 'Show sections'}
+                  {state.showSecList.includes(course.number) ? 'Hide sections' : 'Show sections'}
               </div>
               <div className='number'>{course.number}</div>
               <div className='credits'>
@@ -172,7 +277,7 @@ function FavList(props) {
                 } }>Remove</div>
             </div>
             {
-              showList.showSecList.includes(course.number) ? 
+              state.showSecList.includes(course.number) ? 
                 Object.entries(course.sections).map(([section,sectionInfo]) => (
                   <React.Fragment key={section}>
                     <div className='favSecInfo' >
@@ -188,9 +293,9 @@ function FavList(props) {
                       {
                         Object.keys(sectionInfo.subsections).length !== 0
                         ? <div 
-                            className={ showList.showSubSecList.includes(course.number + '-' + section) ? 'showSectionBtn' : 'showSectionBtn_active' }
+                            className={ state.showSubSecList.includes(course.number + '-' + section) ? 'showSectionBtn' : 'showSectionBtn_active' }
                             onClick={()=>toggleShowSubSecList(course.number, section)}>
-                              {showList.showSubSecList.includes(course.number + '-' + section) ? 'Hide subsections' : 'Show subsections'}
+                              {state.showSubSecList.includes(course.number + '-' + section) ? 'Hide subsections' : 'Show subsections'}
                           </div>
                         : null
                       }
@@ -209,7 +314,7 @@ function FavList(props) {
                         }}>Remove</div>
                     </div>
                     {
-                      showList.showSubSecList.includes(course.number + '-' + section) ? 
+                      state.showSubSecList.includes(course.number + '-' + section) ? 
                         Object.entries(sectionInfo.subsections).map(([subsection,subsectionInfo]) => (
                           <div className='favSubSecInfo' key={subsection}>
                             {
@@ -248,11 +353,15 @@ function FavList(props) {
     </div>
     <div className='footer'>
       <div className='info'>
-        <div>Selected <span>{Object.values(selectedCourseList).length}</span> classes</div>
+        <div>Selected <span>{Object.values(state.selectedCourseList).length}</span> classes</div>
         <div>Total credits: <span>{getTotalCredits()}</span></div>
       </div>
       <div className='settingBtn'></div>
-      <div className='generateBtn'>Generate Schedule</div>
+      <div
+        className = { Object.values(state.selectedCourseList).length ? 'generateBtn' : 'generateBtn_disabled' }
+        onClick={ ()=>{Object.values(state.selectedCourseList).length ? console.log('a') : console.log('b')}}>
+        Generate Schedule
+      </div>
     </div>
   </div>
 }
